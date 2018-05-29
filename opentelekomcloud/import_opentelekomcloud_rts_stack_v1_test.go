924f6124c0c99ee9eca1d720309243a942a4e209
simranjit.nagra <simranjit.nagra@click2cloud.net>
2018-05-29 17:13:05 +0530

    added rts stack import file

---
+++ opentelekomcloud/import_opentelekomcloud_rts_stack_v1_test.go
@@ -0,0 +1,29 @@
+package opentelekomcloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+// PASS
+func TestAccOTCRtsStackV1_importBasic(t *testing.T) {
+	resourceName := "opentelekomcloud_rts_stack_resource_v1.stack_1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOTCRtsStackV1Destroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccRtsStackV1_basic,
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
